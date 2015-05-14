604e1528a859ab356ca3ab7cf9e9780a3a1ecef8
Patrick Michaud <pmichaud@uw.edu>
2015-05-14 08:26:57 -0700

    Fixing pep8 issues, adding jshint

--- sqlshare_web/urls.py
+++ sqlshare_web/urls.py
@@ -1,16 +1,11 @@
 from django.conf.urls import patterns, include, url
 
-#from django.contrib import admin
-#admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    
-    # url(r'^blog/', include('blog.urls')),
-    #url(r'^admin/', include(admin.site.urls)),
-    
-    # include applications
+urlpatterns = patterns(
+    '',
     url(r'^$', 'sqlshare_web.views.dataset_list', name='dataset_list'),
-    url(r'^detail', 'sqlshare_web.views.dataset_detail', name='dataset_detail'),
-    url(r'^upload', 'sqlshare_web.views.dataset_upload', name='dataset_upload'),
+    url(r'^detail', 'sqlshare_web.views.dataset_detail',
+        name='dataset_detail'),
+    url(r'^upload', 'sqlshare_web.views.dataset_upload',
+        name='dataset_upload'),
 )
