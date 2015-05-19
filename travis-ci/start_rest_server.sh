c82a37fb1a57e44db8597877aa308148d2987fd8
Patrick Michaud <pmichaud@uw.edu>
2015-05-19 11:59:53 -0700

    First attempt at running the rest server w/ tests.

---
+++ travis-ci/start_rest_server.sh
@@ -0,0 +1,35 @@
+
+STARTING_DIR=`pwd`
+
+echo $STARTING_DIR
+
+mkdir /tmp/rest_server
+cd /tmp/rest_server
+
+echo "now in `pwd`"
+
+
+git clone https://github.com/uw-it-aca/sqlshare-rest.git
+cd sqlshare-rest
+virtualenv .
+git checkout feature/management-command-client
+source bin/activate
+pip install -r requirements.txt
+pip install PyMySQL
+django-admin.py startproject project .
+cp $STARTING_DIR/travis-ci/rest-server-settings.py project/settings.py
+cp $STARTING_DIR/travis-ci/rest-server-urls.py project/urls.py
+
+python manage.py syncdb --noinput
+
+CLIENT_VALUES=`python manage.py create_client_app --name TestApp --return-url http://localhost --owner-username=test_owner`
+export OAUTH_CLIENT_ID=`echo $CLIENT_VALUES | cut -d ' ' -f 1 | cut -d : -f 2`
+export OAUTH_CLIENT_SECRET=`echo $CLIENT_VALUES | cut -d ' ' -f 2 | cut -d : -f 2`
+
+echo "ID: $OAUTH_CLIENT_ID, $OAUTH_CLIENT_SECRET"
+
+
+python manage.py runserver localhost:9000 &
+
+cd $STARTING_DIR
+
