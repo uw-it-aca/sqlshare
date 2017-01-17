7496647667139aebf8ff962972ccc82689d5fc21
Patrick Michaud <pmichaud@uw.edu>
2017-01-17 13:52:12 -0800

    adding db migrate

--- travis-ci/start_rest_server.sh
+++ travis-ci/start_rest_server.sh
@@ -17,6 +17,7 @@ cp $STARTING_DIR/travis-ci/rest-server-settings.py project/settings.py
 cp $STARTING_DIR/travis-ci/rest-server-urls.py project/urls.py
 
 python manage.py syncdb --noinput
+python manage.py migrate --noinput
 
 CLIENT_VALUES=`python manage.py create_client_app --name TestApp --return-url http://localhost:8000/oauth --owner-username=test_owner`
 export OAUTH_CLIENT_ID=`echo $CLIENT_VALUES | cut -d ' ' -f 1 | cut -d : -f 2`
